2f3bdda53da4e9e1437c27d8538ac2aedcdd66f3
Joshua3212 <49119664+Joshua3212@users.noreply.github.com>
2021-02-23 22:09:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const port = 80;
 const app = express();
 
-app.use(express.static("/data/files/dist/"));
+app.use(express.static("./dist/"));
 app.get(/.*/, function(req, res) {
-  res.sendfile("/data/files/dist/index.html");
+  res.sendfile("./dist/index.html");
 });
 app.listen(port);
 
